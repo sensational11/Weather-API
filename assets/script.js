463f659eac5f428d7010b4f3abc19c8785c4c8bc
sensational.mack <mackn478@gmail.com>
2022-10-10 15:43:54 -0400

    recent changes

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+//Variable Declaration
+var clickSearch = $("searchMe");
+var inputSearch = "";
+var cityLocation = "";
+var dayOfTheWeek = moment().format("MMMMM Do YYY, h:mm:ss a");
+var apiKey = "d899707429dae12637678613a5874634";
+
+clickSearch.click(function () {
+    // console.log("clickSearch");
+    inputSearch = $("searchMe").val().trim();
+    getLocation(inputSearch);
+});
+
+
+function getLocation (inputSearch) {
+    var apiKeyUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputSearch + "&limit=1&appid=" + apiKey;
+    fetch(apiKeyUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                var latitudeLocation = data[0].lat;
+                var longitudeLocation = data[0].lon;
+                cityLocation = data[0].name;
+                var latitudeString = latitudeLocation.toString();
+                var longitudeString = longitudeLocation.toString();
+                getWeather(latitudeString, longitudeString)
+            }
+    )}
+    }
+    )
+}
